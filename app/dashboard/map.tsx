181a1969dcb3fc7dfb85fb26692f24c766582e90
LockBorn1 <161664739+LockBorn1@users.noreply.github.com>
2026-02-09 00:20:30 -0500

    Update map.tsx

--- app/dashboard/map.tsx
+++ app/dashboard/map.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import type { LatLngTuple } from "leaflet";
 
 export default function Map() {
+  const center: LatLngTuple = [39.3292, -82.1013];
+
   return (
-    <MapContainer
-      center={[39.3292, -82.1013]}
-      zoom={13}
-      style={{ height: "100%", width: "100%" }}
-    >
+    <MapContainer center={center} zoom={13} style={{ height: "100%", width: "100%" }}>
       <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[39.3292, -82.1013]}>
+      <Marker position={center}>
         <Popup>Athens RP Patrol Area</Popup>
       </Marker>
     </MapContainer>
