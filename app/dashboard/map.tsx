b5626db4be2d8038c696ccedd63d59ff50658356
LockBorn1 <161664739+LockBorn1@users.noreply.github.com>
2026-02-09 00:24:41 -0500

    Switch to OpenStreetMap iframe for map display
    Replaced react-leaflet map components with an iframe for OpenStreetMap.

--- app/dashboard/map.tsx
+++ app/dashboard/map.tsx
@@ -1,39 +1,12 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-// Disable TypeScript check for this file
-// @ts-nocheck
-
-const MapContainer = dynamic(
-  () => import("react-leaflet").then(m => m.MapContainer),
-  { ssr: false }
-);
-
-const TileLayer = dynamic(
-  () => import("react-leaflet").then(m => m.TileLayer),
-  { ssr: false }
-);
-
-const Marker = dynamic(
-  () => import("react-leaflet").then(m => m.Marker),
-  { ssr: false }
-);
-
-const Popup = dynamic(
-  () => import("react-leaflet").then(m => m.Popup),
-  { ssr: false }
-);
-
 export default function Map() {
-  const center = [39.3292, -82.1013];
-
   return (
-    <MapContainer center={center} zoom={13} style={{ height: "100%", width: "100%" }}>
-      <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={center}>
-        <Popup>Athens RP Patrol Area</Popup>
-      </Marker>
-    </MapContainer>
+    <iframe
+      width="100%"
+      height="100%"
+      style={{ border: 0 }}
+      src="https://www.openstreetmap.org/export/embed.html?bbox=-82.15%2C39.30%2C-82.05%2C39.36&layer=mapnik&marker=39.3292%2C-82.1013"
+    />
   );
 }
